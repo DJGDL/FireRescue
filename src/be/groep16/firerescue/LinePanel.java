ba078a2d07e188082c8b1873dc3262e4e8ec9c89
RafrosDavros <raphael.beerens@gmail.com>
2022-04-08 12:42:10 +0200

    title van in de tekening verandert

--- src/be/groep16/firerescue/LinePanel.java
+++ src/be/groep16/firerescue/LinePanel.java
@@ -14,7 +14,7 @@ public class LinePanel extends JPanel implements ActionListener
         JFrame f = new JFrame();
         f.setSize(500,500);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setTitle("Lijnen tekenen");
+        f.setTitle("Brandweerman Tom");
         f.setLocation(100, 100); //standaard in de hoek van het scherm
         JPanel hoofdpaneel = new LinePanel();
         f.add(hoofdpaneel);

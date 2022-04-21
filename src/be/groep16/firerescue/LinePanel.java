5277a1b5bf51b678817a674a20ec229c71add7df
ArBo79 <Arthur.bortels@vub.be>
2022-04-21 14:47:56 +0200

    Steen foto zonder achtergrond

--- src/be/groep16/firerescue/LinePanel.java
+++ src/be/groep16/firerescue/LinePanel.java
@@ -7,26 +7,25 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class LinePanel extends JPanel implements ActionListener
-{
-    public static void main(String[] args)
-    {
-        JFrame f = new JFrame();
-        f.setSize(500,500);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class LinePanel extends JPanel implements ActionListener {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setSize(500, 500);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        f.setTitle("Brandweerman Sam");
+		f.setTitle("Brandweerman Sam");
 
-        f.setLocation(100, 100); //standaard in de hoek van het scherm
-        JPanel hoofdpaneel = new LinePanel();
-        f.add(hoofdpaneel);
-        f.setVisible(true);
-    }
+		f.setLocation(100, 100); // standaard in de hoek van het scherm
+		JPanel hoofdpaneel = new LinePanel();
+		f.add(hoofdpaneel);
+		f.setVisible(true);
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }

00451e4c1c6c52c69fbb45f97fedaa6c1d1f8e79
RafrosDavros <raphael.beerens@gmail.com>
2022-04-21 14:23:56 +0200

    Format

--- src/be/groep16/firerescue/Gebouw.java
+++ src/be/groep16/firerescue/Gebouw.java
@@ -10,22 +10,20 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Gebouw{
-	Gebouw() 
-	 {
-	  try 
-	  {
-	   JFrame f = new JFrame("Gebouw");
-	   JPanel panel = new JPanel();
-	   panel.setBounds(0, 0, Variabelen.XG, Variabelen.YG);
-	   BufferedImage img = ImageIO.read(new File("gebouw.png"));
-	   JLabel pic = new JLabel(new ImageIcon(img));
-	   panel.add(pic);
-	   f.add(panel);
-	   f.setSize(Variabelen.XG, Variabelen.YG);
-	   f.setLayout(null);
-	   f.setVisible(true);
-	  } 
-	  catch (IOException e) {}
-	 }
+public class Gebouw {
+	Gebouw() {
+		try {
+			JFrame f = new JFrame("Gebouw");
+			JPanel panel = new JPanel();
+			panel.setBounds(0, 0, Variabelen.XG, Variabelen.YG);
+			BufferedImage img = ImageIO.read(new File("gebouw.png"));
+			JLabel pic = new JLabel(new ImageIcon(img));
+			panel.add(pic);
+			f.add(panel);
+			f.setSize(Variabelen.XG, Variabelen.YG);
+			f.setLayout(null);
+			f.setVisible(true);
+		} catch (IOException e) {
+		}
+	}
 }

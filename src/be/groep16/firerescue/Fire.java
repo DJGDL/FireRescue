8e2ac236aadb49b304d48bc143e94f35396b0574
RafrosDavros <raphael.beerens@gmail.com>
2022-04-26 13:05:05 +0200

    Vuurbal geïnplementeert

--- src/be/groep16/firerescue/Fire.java
+++ src/be/groep16/firerescue/Fire.java
@@ -1,31 +1,46 @@
 package be.groep16.firerescue;
 
+import java.awt.Component;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Fire {
+@SuppressWarnings("serial")
+public class Fire extends JPanel implements Entity {
+	private Vector positie;
 	Fire() {
-		try {
-			JFrame f = new JFrame("Vuurbal");
-			JPanel panel = new JPanel();
-			panel.setBounds(0, 0, Variabelen.XF, -500);
-			File vuur = new File("brandweerman.png");
-			System.out.println(vuur.exists());
-			BufferedImage img = ImageIO.read(vuur);
-			JLabel pic = new JLabel(new ImageIcon(img));
-			panel.add(pic);
-			f.add(panel);
-			f.pack();
-			f.setVisible(true);
+		positie = new Vector(Variabelen.XF, Variabelen.Y);
 
+		File fire = new File("Vuurbal.png");
+
+		try {
+			BufferedImage img = ImageIO.read(fire);
+			JLabel pic = new JLabel(new ImageIcon(img));
+			add(pic);
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
+	@Override
+	public void onUpdate(long deltaTime) {
+		positie = positie.add(0, 1);
+
+	}
+
+	@Override
+	public Component getComponent() {
+
+		return this;
+	}
+
+	@Override
+	public Vector getPosition() {
+
+		return positie;
 	}
 }

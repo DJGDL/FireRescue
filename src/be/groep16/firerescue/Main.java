b1145c6ad59aa68c4f19906934c7b8e862edf6da
DJGDL <gwenn.yvan.r.cherifi@vub.ac.be>
2022-05-01 14:03:45 +0200

    Update Main.java

--- src/be/groep16/firerescue/Main.java
+++ src/be/groep16/firerescue/Main.java
@@ -1,5 +1,6 @@
 package be.groep16.firerescue;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -8,15 +9,22 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.border.Border;
 
 @SuppressWarnings("serial")
 public class Main extends JPanel implements ActionListener, KeyListener {
 	private Timer timer;
 	private ArrayList<Entity> entities;
 	private Brandweerman player;
+	
+	int count = 0;
+	JLabel label;
+
 	
 	public Main() {
 		entities = new ArrayList<>();
@@ -38,22 +46,37 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 		player = new Brandweerman();
 		entities.add(player);
 		
+		//player.addActionListener(this);
+		
 	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		
+		JLabel label = new JLabel("score:");
 		JPanel mainPanel = new Main();
 		frame.add(mainPanel);
+		Border border = BorderFactory.createLineBorder(Color.black, 3);
+		
+		label.setBackground(Color.gray);
+		label.setOpaque(true);
+		label.setBorder(border);
+		label.setHorizontalAlignment(JLabel.LEFT);
+		label.setVerticalAlignment(JLabel.CENTER);
+		label.setBounds(5, 5, 75, 35);
 		
 		frame.setMinimumSize(new Dimension(Variabelen.BScherm, Variabelen.HScherm));
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		mainPanel.setPreferredSize(new Dimension(Variabelen.BScherm, Variabelen.HScherm));
-		
+		mainPanel.add(label);
+		mainPanel.setLayout(null);
 		frame.pack();
 	}
+	
+	//public void actionPerformed2(ActionEvent main) {
+		//label.setText("Score: " + count);
+	//}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {

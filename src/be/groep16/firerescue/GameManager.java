476140f0b703a737e3a902709c7fe68b6a7cd998
RafrosDavros <raphael.beerens@gmail.com>
2022-05-04 15:05:55 +0200

    Update GameManager.java

--- src/be/groep16/firerescue/GameManager.java
+++ src/be/groep16/firerescue/GameManager.java
@@ -20,41 +20,40 @@ public class GameManager implements KeyListener {
 
 	private Building building;
 	private Firefighter player;
-	
+
 	private int COUNT_DOWN = 0;
-	
+
 	private int lives = 3;
 	private int score = 0;
 
 	private Entity getNewEntity(int id) {
-		
-			if (nonActiveEntity.get(id).size() > 0) {
-				Entity entity = nonActiveEntity.get(id).pop();
-				entity.reset();
 
-				activeEntity.get(id).add(entity);
+		if (nonActiveEntity.get(id).size() > 0) {
+			Entity entity = nonActiveEntity.get(id).pop();
+			entity.reset();
 
-				return entity;
-			} 
-			else {
-				Entity entity = switch (id) {
-				case ROCK_ID -> new Rock();
-				case FIRE_ID -> new Fire();
-				case DROPLET_ID -> new Droplet();
-				case SMILE_DROPLET_ID -> new SmileDroplet();
-				default -> throw new IllegalArgumentException("Unexpected value: " + id);
-				};
-				activeEntity.get(id).add(entity);
+			activeEntity.get(id).add(entity);
 
-				return entity;
-			}
-		
+			return entity;
+		} else {
+			Entity entity = switch (id) {
+			case ROCK_ID -> new Rock();
+			case FIRE_ID -> new Fire();
+			case DROPLET_ID -> new Droplet();
+			case SMILE_DROPLET_ID -> new SmileDroplet();
+			default -> throw new IllegalArgumentException("Unexpected value: " + id);
+			};
+			activeEntity.get(id).add(entity);
+
+			return entity;
+		}
+
 	}
 
 	public GameManager() {
 		building = new Building();
 		player = new Firefighter();
-		
+
 		activeEntity = new ArrayList<>();
 		nonActiveEntity = new ArrayList<>();
 
@@ -68,7 +67,7 @@ public class GameManager implements KeyListener {
 		COUNT_DOWN -= (int) deltaTime;
 		if (COUNT_DOWN <= 0) {
 			COUNT_DOWN = Variabelen.SPAWN_SPEED;
-			
+
 			int chance = Variabelen.RANDOM.nextInt(11);
 			if (chance < 4)
 				getNewEntity(ROCK_ID);
@@ -78,6 +77,7 @@ public class GameManager implements KeyListener {
 				getNewEntity(DROPLET_ID);
 			if (chance >= 10)
 				getNewEntity(SMILE_DROPLET_ID);
+
 		}
 
 		player.onUpdate(deltaTime);
@@ -92,28 +92,28 @@ public class GameManager implements KeyListener {
 				}
 			}
 		}
-		
+
 		// check collisions
 		for (int id = 0; id < activeEntity.size(); id++) {
 			for (Iterator<Entity> it = activeEntity.get(id).iterator(); it.hasNext();) {
 				Entity e = it.next();
-				
+
 				if (player.getBoundingBox().intersects(e.getBoundingBox())) {
 					if (e instanceof Rock) {
 						lives -= 2;
 					} else if (e instanceof Fire) {
-						lives --;
+						lives--;
 						score -= 5;
 					} else if (e instanceof Droplet) {
 						score += 10;
-						it.remove();
 					} else if (e instanceof SmileDroplet) {
-						lives ++;
+						lives++;
 						score += 20;
 					}
-					
+
 					it.remove();
 					nonActiveEntity.get(id).push(e);
+
 				}
 			}
 		}
